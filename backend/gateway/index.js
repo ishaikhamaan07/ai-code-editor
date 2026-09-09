bf13824cc8f7f1021db61fbfb3250fc00d024c9f
ishaikhamaan07 <ishaikhamaan07@gmail.com>
2026-09-10 03:55:25 +0530

    feat: connect auth service to MongoDB

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -16,7 +16,7 @@ app.use(cors({
 app.use(cookieParser());
 app.use(morgan("dev"));
 
-app.use("/auth",proxy(process.env.AUTH_SERVICE))
+app.use("/api/auth",proxy(process.env.AUTH_SERVICE))
 app.get("/",(req,res)=>{
     res.json({message:"hello from gateway"});
 });
--- backend/services/auth/index.js
+++ backend/services/auth/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import { connectDB } from "./config/db.js";
 dotenv.config();
 const port = process.env.PORT || 8001;
 
@@ -10,5 +11,6 @@ app.get("/",(req,res)=>{
 });
 
 app.listen(port,()=>{
+    connectDB();
     console.log(`auth started at ${port}`);
 });

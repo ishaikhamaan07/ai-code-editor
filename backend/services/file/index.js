ade55f8cc5755f869c9dd4bdd3f60abbe754108b
ishaikhamaan07 <ishaikhamaan07@gmail.com>
2026-09-24 21:29:36 +0530

    feat: implement file service with CRUD operations and tree structure

--- backend/services/file/index.js
+++ backend/services/file/index.js
@@ -1,12 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
 import { connectDB } from "./config/db.js";
+import router from "./routes/file.route.js";
 dotenv.config();
 const port = process.env.PORT || 8003;
 
 const app = express();
 app.use(express.json());
-
+app.use("/",router)
 app.get("/",(req,res)=>{
     res.json({message:"hello from file service"});
 });
